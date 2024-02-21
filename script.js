4d6219caa71219d473dd92f5b2bbd2a5906ef212
andrew <cole.zubac@gmail.com>
2024-02-20 19:07:34 -0700

    more js functionality

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var startbtn = document.querySelector(".start");
+var questions = document.querySelector(".question-container")
+var answers = document.querySelectorAll(".answers")
 
 startbtn.addEventListener("click", startButton);
 
 function startButton() {
     console.log("started");
-    startbtn.classList.add("hide");
+    startbtn.style.display = "none";
     showquestions();
 };  
 
 function showquestions() {
-
+  questions.style.visibility = "visible";
+  answers.forEach(answers => {
+  answers.style.visibility = "visible";
+});
 };
 
 var countdown = function(num) {
